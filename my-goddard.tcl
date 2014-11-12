7b89254e569522dda4b58a96b59fc19eefa7e61f
proshunsuke <shunsuke0901@gmail.com>
2014-11-13 03:56:42 +0900

    全ノードをつないだ

--- my-goddard.tcl
+++ my-goddard.tcl
@@ -188,29 +188,27 @@ proc connectDigestNode { selfIndexNum } {
     global ns digestNode nomalDigestNode nomalNotDigestNode clusterNum notGetDigestNomalNum getDigestNomalNum nomalNodeNum digestNodeNum
     # ダイジェスト未取得ノーマルノード
     for {set i 0} {$i < $digestNodeNum}  {incr i} {
-        for {set j 0} {$j < $notGetDigestNomalNum} {
-            $ns duplex-link $digestNode($selfIndexNum,$i) $nomalNotDigestNode($selfIndexNum,$j]) 10Mb 5ms DropTail
+        for {set j 0} {$j < $notGetDigestNomalNum} {incr j} {
+            $ns duplex-link $digestNode($selfIndexNum,$i) $nomalNotDigestNode($selfIndexNum,$j) 10Mb 5ms DropTail
         }
     }
 }
 
-proc connectNomalNode { } {
+proc connectNomalNode { selfIndexNum } {
     global ns connectNomalNodeRate nomalDigestNode nomalNotDigestNode clusterNum notGetDigestNomalNum getDigestNomalNum nomalNodeNum
-    set nomalNodeList(0) ""
-
     # とりあえずリストに全部入れる
     for {set i 0} {$i < $nomalNodeNum} {incr i} {
         if {$i >= $notGetDigestNomalNum} {
-            $nomalNodeList($i) = $nomalDigestNode($selfIndexNum,[expr $i-$notGetDigestNomalNum])
+            set nomalNodeList($i) $nomalDigestNode($selfIndexNum,[expr $i-$notGetDigestNomalNum])
         } else {
-            $nomalNodeList($i) = $nomalNotDigestNode($selfIndexNum,)
+            set nomalNodeList($i) $nomalNotDigestNode($selfIndexNum,$i)
         }
     }
 
     # 適当な回数リストの中身をシャッフル
     set temp ""
     for {set i 0} {$i < 100 } {incr i} {
-        set randomNum [expr int(($nomalNodeNum)*rand())]
+        set randomNum1 [expr int(($nomalNodeNum)*rand())]
         set randomNum2 [expr int(($nomalNodeNum)*rand())]
         set temp $nomalNodeList($randomNum1)
         set $nomalNodeList($randomNum1) $nomalNodeList($randomNum2)
@@ -222,10 +220,10 @@ proc connectNomalNode { } {
     # ノーマルノード同士：０→１　０→２　０→３　０→４、１→２　１→３...１４→１５　１４→０　１４→１　１４→２
     for {set i 0} {$i < $nomalNodeNum} {incr i} {
         for {set j 0} {$j < $connectNomalNum} {incr j} {
-            if { [expr $i+$j] >= $nomalNodeNum } {
-                $ns duplex-link $nomalNodeList($i) $nomalNodeList([expr $i+$j-$nomalNodeNum]) 10Mb 5ms DropTail
+            if { [expr $i+$j+1] >= $nomalNodeNum } {
+                $ns duplex-link $nomalNodeList($i) $nomalNodeList([expr $i+$j+1-$nomalNodeNum]) 10Mb 5ms DropTail
             } else {
-                $ns duplex-link $nomalNodeList($i) $nomalNodeList([expr $i+$j]) 10Mb 5ms DropTail
+                $ns duplex-link $nomalNodeList($i) $nomalNodeList([expr $i+$j+1]) 10Mb 5ms DropTail
             }
         }
     }
@@ -333,4 +331,4 @@ proc finish {} {
     # exit 0
 }
 
-# $ns run
+$ns run
